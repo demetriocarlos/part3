03c1044d41845a9efaf578bd65e8437c9c388d9e
carlos <carlosdemetrio390@gmail.com>
2024-05-03 15:27:47 -0600

    modificando array persons

--- backend_de_la_agenda_Telefonica/index.js
+++ backend_de_la_agenda_Telefonica/index.js
@@ -23,29 +23,7 @@ app.use((req, res, next) => {
   next();
 });
 
-
-const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+ 
 
 const  getCurrentTimeGMT = () => {
   const now = new Date();
